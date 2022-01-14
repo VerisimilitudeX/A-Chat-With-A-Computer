7340cc8d73840845fd2c23032098d2a744269395
Verisimilitude <96280466+Verisimilitude11@users.noreply.github.com>
2022-01-14 12:13:50 -0800

    Add files via upload

---
+++ A_Chat_With_A_Computer.py
@@ -0,0 +1,69 @@
+"""
+LESSON: 1.1 - Statements & Variables
+EXERCISE: Computer Chat
+"""
+
+#### ---- QUESTION 1 ---- ####
+
+# Get INPUT with prompt "What is your name? "
+# ASSIGN to variable called name.
+name = input("What is your name? ")
+
+# CONCATENATE variable called name to "Hello ", and
+# concatenate "." to the end. Put entire thing
+# inside of a PRINT statement.
+# ---> TEST AFTER THIS LINE <--- #
+print("Hello " + name + "!")
+
+
+#### ---- QUESTION 2 ---- ####
+
+# Get INPUT with prompt "How are you today? "
+input("How are you today? ")
+
+# PRINT "I see."
+# Then PRINT "I myself am a little nervous,
+# because I am meeting so many new people."
+# ---> TEST AFTER THIS LINE <--- #
+print("I see, " + name)
+print("I myself am a little nervous, because I am meeting so many new people.")
+
+
+
+#### ---- QUESTION 3 ---- ####
+
+# Get INPUT with prompt "What do you like to do for
+# fun, " + name variable + "? "
+input("What do you like to do for fun, " + name + "?")
+
+# PRINT "That sounds like a lot of fun."
+# ---> TEST AFTER THIS LINE <--- #
+print("That sounds interesting")
+
+
+#### ---- QUESTION 4 ---- ####
+
+# PRINT "I'd like to know some more about you."
+print("I'd like to know some more about you.")
+
+# Get INPUT with prompt "What is your favorite color? "
+# and ASSIGN to a variable called color
+color = input("What is your favorite color? ")
+
+# PRINT "It's funny you should say that! I just got a new "
+# + color variable + " keyboard the other day."
+# ---> TEST AFTER THIS LINE <--- #
+print("It's funny you should say that! I just got a new " + color + " keyboard the other day.")
+
+
+#### ---- QUESTION 5 ---- ####
+
+# Get INPUT with prompt "Will you be my friend? "
+input("Will you be my friend? ")
+
+# PRINT "Well, good."
+# ---> TEST AFTER THIS LINE <--- #
+print("OK")
+
+# Turn in your Coding Exercise.
+
