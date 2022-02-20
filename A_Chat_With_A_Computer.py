e1af2b87481293e7be72b1adde34f4fac7d9be2e
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 16:55:27 -0800

    Update A_Chat_With_A_Computer.py

--- A_Chat_With_A_Computer.py
+++ A_Chat_With_A_Computer.py
@@ -1,18 +1,12 @@
-
 name = input("What is your name? ")
-
 print("Hello " + name + "!")
 input("How are you today? ")
-
 print("I see, " + name)
 print("I myself am a little nervous, because I am meeting so many new people.")
 input("What do you like to do for fun, " + name + "?")
-
 print("That sounds interesting")
 print("I'd like to know some more about you.")
-
 color = input("What is your favorite color? ")
 print("It's funny you should say that! I just got a new " + color + " keyboard the other day.")
-
 input("Will you be my friend? ")
 print("OK")
